1aadaf04f341e9ce26c1937e2d3d8b39505e9e89
PhongDo750 <dogiaphong2k4@gmail.com>
2025-03-23 21:12:30 +0700

    [add] docs

---
+++ src/main/java/com/example/ttcs_jobwebsite/TtcsJobwebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.ttcs_jobwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtcsJobwebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtcsJobwebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ttcs_jobwebsite/TtcsJobwebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ttcs_jobwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtcsJobwebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
