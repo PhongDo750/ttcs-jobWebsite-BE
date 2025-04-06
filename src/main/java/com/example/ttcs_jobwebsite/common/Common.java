81e8120b21d4811a7cf5a7837db09d3c42153174
PhongDo750 <dogiaphong2k4@gmail.com>
2025-04-06 16:50:09 +0700

    [update] docs

--- src/main/java/com/example/ttcs_jobwebsite/common/Common.java
+++ src/main/java/com/example/ttcs_jobwebsite/common/Common.java
@@ -2,4 +2,6 @@ package com.example.ttcs_jobwebsite.common;
 
 public class Common {
     public static final String IMAGE_DEFAULT = "http://res.cloudinary.com/dxdndamgg/image/upload/v1729500485/slerkr495gfnca9sdpq0.png";
+    public static final String USER = "USER";
+    public static final String RECRUITER = "RECRUITER";
 }
